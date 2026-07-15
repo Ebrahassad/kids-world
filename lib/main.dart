6b910a6ee461623a9c1f3ac9b3680af68aaaf456
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-16 00:54:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,6 @@
 import 'package:flutter/material.dart';
+import 'app.dart';
 
 void main() {
   runApp(const KidsWorldApp());
-}
-
-class KidsWorldApp extends StatelessWidget {
-  const KidsWorldApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kids World',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue,
-        ),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Kids World'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'Welcome to Kids World!',
-          style: TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
 }
