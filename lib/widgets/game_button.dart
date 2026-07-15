33eb9b670d19f83b667ec726f68c7ae8209bf6de
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-16 01:03:51 +0200

    Create game_button.dart

---
+++ lib/widgets/game_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class GameButton extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const GameButton({
+    super.key,
+    required this.title,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 220,
+      height: 55,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(14),
+          ),
+        ),
+        child: Text(
+          title,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
