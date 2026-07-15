18dd6f78a24c491ecb320ab7acc7f82b3919eb66
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-16 00:57:21 +0200

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String appName = 'Kids World';
+
+  static const String welcomeTitle = 'Welcome to Kids World';
+
+  static const String welcomeSubtitle =
+      'Fun Puzzle Game for Kids';
+}
