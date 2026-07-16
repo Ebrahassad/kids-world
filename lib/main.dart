d3d3a84e84e9bc5bb9c90f719ff598792fa0549a
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-16 21:01:47 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+import 'app.dart';
 
-class AppRoutes {
-  static const String home = '/';
-
-  static Map<String, WidgetBuilder> routes = {
-    home: (context) => const HomeScreen(),
-  };
+void main() {
+  runApp(const KidsWorldApp());
 }
