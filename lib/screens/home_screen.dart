60f00050ffa9c6a7234fa2551650ebb4b3f0db2c
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-16 21:35:42 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'game_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -12,7 +13,14 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const GameScreen(),
+              ),
+            );
+          },
           child: const Text(
             'Start Game',
             style: TextStyle(fontSize: 20),
