7e64e28d9dfc2070e5abf4ec9ffa04be5321f2ef
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-17 06:38:38 +0200

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,218 +7,120 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SafeArea(
-
         child: Container(
-
           decoration: const BoxDecoration(
-
             image: DecorationImage(
-
               image: AssetImage(
                 "assets/images/Picture/background.png",
               ),
-
               fit: BoxFit.cover,
-
             ),
-
           ),
 
-
           child: Center(
-
             child: SingleChildScrollView(
-
               child: Column(
-
                 mainAxisAlignment: MainAxisAlignment.center,
-
                 children: [
 
-
-                  // شعار التطبيق
-
                   Container(
-
                     width: 170,
-
                     height: 170,
 
-
                     decoration: BoxDecoration(
-
                       color: Colors.white,
-
                       shape: BoxShape.circle,
 
-
                       boxShadow: [
-
                         BoxShadow(
-
                           color: Colors.blueAccent,
-
                           blurRadius: 20,
-
                           spreadRadius: 5,
-
                         ),
-
                       ],
-
                     ),
-
 
                     child: Center(
-
                       child: ClipOval(
-
                         child: Image.asset(
-
                           "assets/icon/app_icon.png",
-
                           width: 150,
-
                           height: 150,
-
                           fit: BoxFit.cover,
-
                         ),
-
                       ),
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 30),
 
-
-
                   const Text(
-
                     "Kids World",
-
                     style: TextStyle(
-
                       fontSize: 38,
-
                       fontWeight: FontWeight.bold,
-
                       color: Colors.blue,
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 10),
 
-
-
                   const Text(
-
                     "عالم الاطفال",
-
                     style: TextStyle(
-
                       fontSize: 26,
-
                       fontWeight: FontWeight.bold,
-
                       color: Colors.black87,
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 12),
 
-
-
                   const Text(
-
                     "تعلم ⭐ العب ⭐ اجمع النجوم",
-
                     style: TextStyle(
-
                       fontSize: 20,
-
                       color: Colors.black54,
-
                     ),
-
                   ),
-
-
 
                   const SizedBox(height: 45),
 
-
-
                   SizedBox(
-
                     width: 260,
-
                     height: 65,
 
-
                     child: ElevatedButton(
-
                       style: ElevatedButton.styleFrom(
-
                         backgroundColor: Colors.green,
-
                         elevation: 6,
 
-
                         shape: RoundedRectangleBorder(
-
                           borderRadius: BorderRadius.circular(30),
-
                         ),
-
                       ),
-
 
                       onPressed: () {
 
-
                         Navigator.push(
-
                           context,
 
                           MaterialPageRoute(
-
-                            builder: (_) => const LevelsScreen(),
-
+                            builder: (_) => LevelsScreen(),
                           ),
 
                         );
 
-
                       },
 
-
                       child: const Text(
-
                         "ابدأ من هنا 🎮",
 
                         style: TextStyle(
-
                           fontSize: 28,
-
                           color: Colors.white,
-
                           fontWeight: FontWeight.bold,
-
                         ),
 
                       ),
@@ -227,21 +129,12 @@ class HomeScreen extends StatelessWidget {
 
                   ),
 
-
                 ],
-
               ),
-
             ),
-
           ),
-
         ),
-
       ),
-
     );
-
   }
-
 }
