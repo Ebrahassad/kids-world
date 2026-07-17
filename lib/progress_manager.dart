d745844281d3626366c9353801b8ae6335532d69
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-18 01:48:41 +0200

    Update progress_manager.dart

--- lib/progress_manager.dart
+++ lib/progress_manager.dart
@@ -2,7 +2,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ProgressManager {
 
-  // حفظ مكان اللاعب داخل اللعبة
   static Future<void> saveProgress(
     String gameName,
     int question,
@@ -14,11 +13,9 @@ class ProgressManager {
       gameName,
       question,
     );
-
   }
 
 
-  // جلب مكان اللاعب داخل اللعبة
   static Future<int> getProgress(
     String gameName,
   ) async {
@@ -26,34 +23,9 @@ class ProgressManager {
     final prefs = await SharedPreferences.getInstance();
 
     return prefs.getInt(gameName) ?? 0;
-
   }
 
 
-  // حفظ مجموع النجوم
-  static Future<void> saveStars(int stars) async {
-
-    final prefs = await SharedPreferences.getInstance();
-
-    await prefs.setInt(
-      "total_stars",
-      stars,
-    );
-
-  }
-
-
-  // جلب مجموع النجوم
-  static Future<int> getStars() async {
-
-    final prefs = await SharedPreferences.getInstance();
-
-    return prefs.getInt("total_stars") ?? 0;
-
-  }
-
-
-  // حفظ اللعبة المكتملة
   static Future<void> saveCompletedGame(
     String gameName,
   ) async {
@@ -61,26 +33,24 @@ class ProgressManager {
     final prefs = await SharedPreferences.getInstance();
 
     await prefs.setBool(
-      gameName,
+      "${gameName}_completed",
       true,
     );
-
   }
 
 
-  // معرفة هل اللعبة مكتملة
   static Future<bool> isGameCompleted(
     String gameName,
   ) async {
 
     final prefs = await SharedPreferences.getInstance();
 
-    return prefs.getBool(gameName) ?? false;
-
+    return prefs.getBool(
+      "${gameName}_completed",
+    ) ?? false;
   }
 
 
-  // مسح تقدم لعبة
   static Future<void> resetProgress(
     String gameName,
   ) async {
@@ -88,17 +58,6 @@ class ProgressManager {
     final prefs = await SharedPreferences.getInstance();
 
     await prefs.remove(gameName);
-
-  }
-
-
-  // مسح كل التقدم
-  static Future<void> resetAll() async {
-
-    final prefs = await SharedPreferences.getInstance();
-
-    await prefs.clear();
-
   }
 
 }
