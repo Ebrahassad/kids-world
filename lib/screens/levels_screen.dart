d87f9d6475f03a4aa057a8a4bbb436dc427055aa
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-17 04:58:53 +0200

    Update levels_screen.dart

--- lib/screens/levels_screen.dart
+++ lib/screens/levels_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'game_screen.dart';
 import 'choose_image_screen.dart';
 import 'match_image_screen.dart';
+import 'colors_screen.dart';
 
 class LevelsScreen extends StatelessWidget {
   const LevelsScreen({super.key});
@@ -38,17 +39,14 @@ class LevelsScreen extends StatelessWidget {
 
       body: Padding(
         padding: const EdgeInsets.all(20),
-
         child: GridView.builder(
           itemCount: levels.length,
-
           gridDelegate:
               const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2,
             crossAxisSpacing: 15,
             mainAxisSpacing: 15,
           ),
-
           itemBuilder: (context, index) {
             return ElevatedButton(
               style: ElevatedButton.styleFrom(
@@ -57,9 +55,7 @@ class LevelsScreen extends StatelessWidget {
                   borderRadius: BorderRadius.circular(25),
                 ),
               ),
-
               onPressed: () {
-
                 if (index == 0) {
                   Navigator.push(
                     context,
@@ -67,51 +63,44 @@ class LevelsScreen extends StatelessWidget {
                       builder: (_) => const GameScreen(),
                     ),
                   );
-                }
-
-                else if (index == 1) {
+                } else if (index == 1) {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (_) => const ChooseImageScreen(),
                     ),
                   );
-                }
-
-                else if (index == 2) {
+                } else if (index == 2) {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (_) => const MatchImageScreen(),
                     ),
                   );
-                }
-
-                else {
+                } else if (index == 3) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => const ColorsScreen(),
+                    ),
+                  );
+                } else {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text(
-                        "هذه المرحلة قيد التطوير 🚀",
-                      ),
+                      content: Text("هذه المرحلة قيد التطوير 🚀"),
                     ),
                   );
                 }
-
               },
-
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-
                 children: [
-
                   const Icon(
                     Icons.star,
                     color: Colors.yellow,
                     size: 45,
                   ),
-
                   const SizedBox(height: 10),
-
                   Text(
                     levels[index],
                     textAlign: TextAlign.center,
@@ -121,7 +110,6 @@ class LevelsScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-
                 ],
               ),
             );
