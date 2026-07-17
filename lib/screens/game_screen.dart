93d9ef851f83a26b0e8655b897c6312d3c02220e
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-17 15:57:08 +0200

    Update game_screen.dart

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,206 +1,1124 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
 import 'win_screen.dart';
 
+
 class GameScreen extends StatefulWidget {
+
   const GameScreen({super.key});
+
 
   @override
   State<GameScreen> createState() => _GameScreenState();
+
 }
 
+
+
 class _GameScreenState extends State<GameScreen> {
+
+
   final AudioPlayer audioPlayer = AudioPlayer();
 
-  final List<Map<String, dynamic>> questions = [
+  final Random random = Random();
+
+
+
+  final List<Map<String, dynamic>> originalQuestions = [
+
+
     {
       "image": "assets/images/Puzzle/kids_puzzle.png",
       "answer": "أطفال",
-      "options": ["أطفال", "قرد", "أسد", "سمكة"],
+      "options": [
+        "أطفال",
+        "قرد",
+        "أسد",
+        "سمكة",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/monkey_puzzle.png",
       "answer": "قرد",
-      "options": ["دب", "قرد", "قطة", "كلب"],
+      "options": [
+        "دب",
+        "قرد",
+        "قطة",
+        "كلب",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/bear_puzzle.png",
       "answer": "دب",
-      "options": ["سمكة", "دب", "أرنب", "أسد"],
+      "options": [
+        "سمكة",
+        "دب",
+        "أرنب",
+        "أسد",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/fish_puzzle.png",
       "answer": "سمكة",
-      "options": ["فراشة", "سمكة", "كلب", "ديناصور"],
+      "options": [
+        "فراشة",
+        "سمكة",
+        "كلب",
+        "ديناصور",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/dinosaur_puzzle.png",
       "answer": "ديناصور",
-      "options": ["قرد", "ديناصور", "أسد", "دب"],
+      "options": [
+        "قرد",
+        "ديناصور",
+        "أسد",
+        "دب",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/butterfly_puzzle.png",
       "answer": "فراشة",
-      "options": ["قطة", "كلب", "فراشة", "سمكة"],
+      "options": [
+        "قطة",
+        "كلب",
+        "فراشة",
+        "سمكة",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/dog_puzzle.png",
       "answer": "كلب",
-      "options": ["كلب", "أرنب", "دب", "أسد"],
+      "options": [
+        "كلب",
+        "أرنب",
+        "دب",
+        "أسد",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/cat_puzzle.png",
       "answer": "قطة",
-      "options": ["قطة", "سمكة", "قرد", "ديناصور"],
+      "options": [
+        "قطة",
+        "سمكة",
+        "قرد",
+        "ديناصور",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/rabbit_puzzle.png",
       "answer": "أرنب",
-      "options": ["فراشة", "كلب", "أرنب", "أسد"],
+      "options": [
+        "فراشة",
+        "كلب",
+        "أرنب",
+        "أسد",
+      ],
     },
+
+
     {
       "image": "assets/images/Puzzle/lion_puzzle.png",
       "answer": "أسد",
-      "options": ["دب", "أسد", "قطة", "سمكة"],
+      "options": [
+        "دب",
+        "أسد",
+        "قطة",
+        "سمكة",
+      ],
     },
+
   ];
 
+
+
+  late List<Map<String, dynamic>> questions;
+
+
+
   int currentQuestion = 0;
+
   int stars = 0;
 
-  void playSound(String fileName) {
-    audioPlayer.play(
-      AssetSource('sounds/$fileName'),
-    );
+
+  bool answered = false;
+
+
+  bool loadingNextQuestion = false;
+  @override
+  void initState() {
+
+    super.initState();
+
+    prepareGame();
+
   }
+
+
+
+  void prepareGame() {
+
+  questions = originalQuestions.map((item) {
+
+    return {
+
+      "image": item["image"],
+      "answer": item["answer"],
+      "options": List<String>.from(
+        item["options"],
+      ),
+
+    };
+
+  }).toList();
+
+
+  questions.shuffle(random);
+
+
+
+
+
+  for (var item in questions) {
+
+    List<String> options =
+        List<String>.from(item["options"]);
+
+    options.shuffle(random);
+
+    item["options"] = options;
+
+  }
+
+}
+
+
+
+
+  Future<void> playSound(String fileName) async {
+
+
+    try {
+
+
+      await audioPlayer.stop();
+
+
+      await audioPlayer.play(
+
+        AssetSource(
+          "sounds/$fileName",
+        ),
+
+      );
+
+
+    } catch (e) {
+
+
+      debugPrint(
+        "خطأ الصوت: $e",
+      );
+
+
+    }
+
+
+  }
+
+
+
+
 
   void checkAnswer(String answer) {
-    if (answer == questions[currentQuestion]["answer"]) {
-      playSound("correct.mp3");
+
+
+    // منع الضغط المتكرر
+
+    if (answered || loadingNextQuestion) {
+
+      return;
+
+    }
+
+
+
+    setState(() {
+
+      answered = true;
+
+    });
+
+
+
+
+    final correctAnswer =
+        questions[currentQuestion]["answer"];
+
+
+
+
+    if (answer == correctAnswer) {
+
+
+
+      playSound(
+        "correct.mp3",
+      );
+
+
 
       setState(() {
+
+
         stars++;
+
+
       });
 
-      if (currentQuestion < questions.length - 1) {
-        setState(() {
-          currentQuestion++;
-        });
-      } else {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(
-            builder: (_) => const WinScreen(),
-          ),
-        );
-      }
-    } else {
-      playSound("wrong.mp3");
+
+
 
       ScaffoldMessenger.of(context).showSnackBar(
+
+
         const SnackBar(
-          content: Text("حاول مرة أخرى ⭐"),
+
+
+          content: Text(
+            "إجابة صحيحة ⭐🎉",
+          ),
+
+
+          duration: Duration(
+            milliseconds: 700,
+          ),
+
+
         ),
+
+
       );
+
+
+
+      goNextQuestion();
+
+
+
+    } else {
+
+
+
+      playSound(
+        "wrong.mp3",
+      );
+
+
+
+
+      ScaffoldMessenger.of(context).showSnackBar(
+
+
+        const SnackBar(
+
+
+          content: Text(
+            "حاول مرة أخرى ⭐",
+          ),
+
+
+          duration: Duration(
+            milliseconds: 800,
+          ),
+
+
+        ),
+
+
+      );
+
+
+
+
+      Future.delayed(
+
+
+        const Duration(
+          milliseconds: 800,
+        ),
+
+
+        () {
+
+
+          if (!mounted) return;
+
+
+
+          setState(() {
+
+
+            answered = false;
+
+
+          });
+
+
+
+        },
+
+
+      );
+
+
+
     }
+
+
   }
+
+
+
+
+
+  void goNextQuestion() async {
+
+
+
+    setState(() {
+
+
+      loadingNextQuestion = true;
+
+
+    });
+
+
+
+    
+    await Future.delayed(
+
+      const Duration(
+        seconds: 1,
+      ),
+
+    );
+
+
+
+    if (!mounted) return;
+
+
+
+    if (currentQuestion < questions.length - 1) {
+
+
+
+      setState(() {
+
+
+        currentQuestion++;
+
+
+        answered = false;
+
+
+        loadingNextQuestion = false;
+
+
+
+      });
+
+
+
+   } else {
+
+  setState(() {
+
+    loadingNextQuestion = false;
+
+  });
+
+
+  if (!mounted) return;
+
+
+  Navigator.pushReplacement(
+  context,
+
+  MaterialPageRoute(
+
+    builder: (_) => WinScreen(
+      stars: stars,
+    ),
+
+  ),
+);
+
+}
+
+  }
+
+
+
+
+
+  void restartGame() {
+
+
+    prepareGame();
+
+
+
+    setState(() {
+
+
+      currentQuestion = 0;
+
+
+      stars = 0;
+
+
+      answered = false;
+
+
+      loadingNextQuestion = false;
+
+
+    });
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    if (questions.isEmpty) {
+
+
+      return const Scaffold(
+
+        body: Center(
+
+          child: CircularProgressIndicator(),
+
+        ),
+
+      );
+
+
+    }
+
+
+
+    final data = questions[currentQuestion];
+
+
+
+    return Scaffold(
+
+
+      body: Container(
+
+
+        width: double.infinity,
+
+
+        height: double.infinity,
+
+
+
+        decoration: const BoxDecoration(
+
+
+          gradient: LinearGradient(
+
+
+            colors: [
+
+
+              Color(0xffFFF59D),
+
+
+              Color(0xff81D4FA),
+
+
+            ],
+
+
+            begin: Alignment.topCenter,
+
+
+            end: Alignment.bottomCenter,
+
+
+          ),
+
+
+        ),
+
+
+
+
+        child: SafeArea(
+
+
+          child: Column(
+
+
+            children: [
+
+
+
+              const SizedBox(height: 15),
+
+
+
+
+              Row(
+
+
+                mainAxisAlignment:
+                    MainAxisAlignment.spaceAround,
+
+
+
+                children: [
+
+
+
+                  Text(
+
+
+                    "⭐ $stars",
+
+
+
+                    style: const TextStyle(
+
+
+                      fontSize: 26,
+
+
+                      fontWeight: FontWeight.bold,
+
+
+                      color: Colors.orange,
+
+
+                    ),
+
+
+                  ),
+
+
+
+
+                  Text(
+
+
+                    "السؤال ${currentQuestion + 1}/${questions.length}",
+
+
+
+                    style: const TextStyle(
+
+
+                      fontSize: 22,
+
+
+                      fontWeight: FontWeight.bold,
+
+
+                    ),
+
+
+                  ),
+
+
+
+                ],
+
+
+              ),
+
+
+
+
+              const SizedBox(height: 20),
+
+
+
+
+              Container(
+
+
+                margin: const EdgeInsets.symmetric(
+
+                  horizontal: 20,
+
+                ),
+
+
+
+                padding: const EdgeInsets.all(15),
+
+
+
+
+                decoration: BoxDecoration(
+
+
+                  color: Colors.white,
+
+
+                  borderRadius:
+                      BorderRadius.circular(25),
+
+
+
+
+                  boxShadow: const [
+
+
+                    BoxShadow(
+
+
+                      color: Colors.black26,
+
+
+                      blurRadius: 8,
+
+
+                      offset: Offset(0, 4),
+
+
+                    ),
+
+
+                  ],
+
+
+
+                ),
+
+
+
+
+                child: Image.asset(
+
+
+                  data["image"],
+
+
+
+                  height:
+
+                      MediaQuery.of(context)
+                          .size
+                          .height * 0.27,
+
+
+
+                  fit: BoxFit.contain,
+
+
+
+                  errorBuilder:
+                      (context, error, stackTrace) {
+
+
+
+                    return const Icon(
+
+
+                      Icons.image_not_supported,
+
+
+                      size: 120,
+
+
+                      color: Colors.grey,
+
+
+                    );
+
+
+
+                  },
+
+
+                ),
+
+
+
+              ),
+
+
+
+
+              const SizedBox(height: 20),
+
+
+
+
+              const Text(
+
+
+                "اختر الإجابة الصحيحة 🎯",
+
+
+
+                style: TextStyle(
+
+
+                  fontSize: 24,
+
+
+                  fontWeight: FontWeight.bold,
+
+
+                ),
+
+
+              ),
+
+
+
+
+              const SizedBox(height: 15),
+
+
+
+
+              Expanded(
+
+
+                child: GridView.builder(
+
+
+                  padding:
+                      const EdgeInsets.symmetric(
+
+                        horizontal: 20,
+
+                      ),
+
+
+
+
+                  itemCount:
+                      data["options"].length,
+
+
+
+
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+
+
+
+                    crossAxisCount: 2,
+
+
+
+                    crossAxisSpacing: 15,
+
+
+
+                    mainAxisSpacing: 15,
+
+
+
+                    childAspectRatio: 2,
+
+
+
+                  ),
+
+
+
+
+                  itemBuilder: (context, index) {
+
+
+                    final String option =
+                        data["options"][index];
+
+
+
+                    return InkWell(
+
+  splashColor: Colors.blue.shade100,
+
+  onTap: (loadingNextQuestion || answered)
+    ? null
+    : () {
+
+        checkAnswer(option);
+
+      },                 
+
+
+
+                      borderRadius:
+                          BorderRadius.circular(20),
+
+
+
+
+                      child: Container(
+
+
+                        alignment:
+                            Alignment.center,
+
+
+
+
+                        decoration: BoxDecoration(
+
+
+                          color: Colors.white,
+
+
+
+                          borderRadius:
+                              BorderRadius.circular(20),
+
+
+
+                          boxShadow: const [
+
+
+
+                            BoxShadow(
+
+
+                              color: Colors.black26,
+
+
+                              blurRadius: 5,
+
+
+                              offset: Offset(0, 3),
+
+
+                            ),
+
+
+
+                          ],
+
+
+
+                        ),
+
+
+
+
+                        child: Text(
+
+
+                          option,
+
+
+
+                          style: const TextStyle(
+
+
+                            fontSize: 22,
+
+
+                            fontWeight: FontWeight.bold,
+
+
+                          ),
+
+
+
+                          textAlign:
+                              TextAlign.center,
+
+
+
+                        ),
+
+
+
+                      ),
+
+
+
+                    );
+
+
+                  },
+
+
+                ),
+
+
+
+              ),
+              const SizedBox(height: 15),
+
+
+
+              ElevatedButton.icon(
+
+
+                onPressed: loadingNextQuestion
+    ? null
+    : restartGame,
+
+
+                icon: const Icon(
+
+                  Icons.refresh,
+
+                  size: 28,
+
+                ),
+
+
+
+                label: const Text(
+
+
+                  "إعادة اللعب",
+
+
+
+                  style: TextStyle(
+
+
+                    fontSize: 20,
+
+
+                    fontWeight: FontWeight.bold,
+
+
+                  ),
+
+
+
+                ),
+
+
+
+
+                style: ElevatedButton.styleFrom(
+
+
+
+                  padding: const EdgeInsets.symmetric(
+
+
+
+                    horizontal: 35,
+
+
+                    vertical: 12,
+
+
+
+                  ),
+
+
+
+
+                  backgroundColor: Colors.white,
+
+
+
+                  foregroundColor: Colors.blue,
+
+
+
+
+                  shape: RoundedRectangleBorder(
+
+
+
+                    borderRadius:
+                        BorderRadius.circular(25),
+
+
+
+                  ),
+
+
+
+                ),
+
+
+
+              ),
+
+
+
+
+              const SizedBox(height: 20),
+
+
+
+            ],
+
+
+          ),
+
+
+        ),
+
+
+      ),
+
+
+    );
+
+
+  }
+
+
+
+
 
   @override
   void dispose() {
+
+
     audioPlayer.dispose();
+
+
+
     super.dispose();
+
+
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final data = questions[currentQuestion];
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "المرحلة ${currentQuestion + 1} / ${questions.length}",
-        ),
-        centerTitle: true,
 
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-
-            child: Row(
-              children: [
-                const Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                ),
-
-                const SizedBox(width: 5),
-
-                Text(
-                  "$stars",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-
-        child: Column(
-          children: [
-
-            Expanded(
-              child: Image.asset(
-                data["image"],
-                fit: BoxFit.contain,
-              ),
-            ),
-
-            const SizedBox(height: 20),
-
-            ...List.generate(
-              (data["options"] as List).length,
-
-              (index) {
-                return Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 6),
-
-                  child: SizedBox(
-                    width: double.infinity,
-                    height: 55,
-
-                    child: ElevatedButton(
-                      onPressed: () {
-                        checkAnswer(
-                          data["options"][index],
-                        );
-                      },
-
-                      child: Text(
-                        data["options"][index],
-
-                        style: const TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-
-            const SizedBox(height: 20),
-          ],
-        ),
-      ),
-    );
-  }
 }
