a455d610a03894a073ecf095e233cdd32cf060df
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-18 02:08:21 +0200

    Update progress_manager.dart

--- lib/progress_manager.dart
+++ lib/progress_manager.dart
@@ -1,34 +1,95 @@
-import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProgressManager {
 
+  // حفظ تقدم اللعبة (رقم السؤال أو المرحلة)
   static Future<void> saveProgress(
     String gameName,
-    int question,
+    int value,
   ) async {
 
     final prefs = await SharedPreferences.getInstance();
 
     await prefs.setInt(
-      gameName,
-      question,
+      "${gameName}_progress",
+      value,
     );
 
   }
 
 
+  // جلب تقدم اللعبة
   static Future<int> getProgress(
     String gameName,
   ) async {
 
     final prefs = await SharedPreferences.getInstance();
 
-    return prefs.getInt(gameName) ?? 0;
+    return prefs.getInt(
+      "${gameName}_progress",
+    ) ?? 0;
 
   }
 
 
+  // حفظ نجوم اللعبة
+  static Future<void> saveStars(
+    String gameName,
+    int stars,
+  ) async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt(
+      "${gameName}_stars",
+      stars,
+    );
+
+  }
+
+
+  // جلب نجوم اللعبة
+  static Future<int> getStars(
+    String gameName,
+  ) async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getInt(
+      "${gameName}_stars",
+    ) ?? 0;
+
+  }
+
+
+  // حفظ مجموع النجوم لكل الألعاب
+  static Future<void> saveTotalStars(
+    int stars,
+  ) async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt(
+      "total_stars",
+      stars,
+    );
+
+  }
+
+
+  // جلب مجموع النجوم
+  static Future<int> getTotalStars() async {
+
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getInt(
+      "total_stars",
+    ) ?? 0;
+
+  }
+
+
+  // تسجيل أن اللعبة اكتملت
   static Future<void> saveCompletedGame(
     String gameName,
   ) async {
@@ -43,6 +104,7 @@ class ProgressManager {
   }
 
 
+  // هل اللعبة مكتملة؟
   static Future<bool> isGameCompleted(
     String gameName,
   ) async {
@@ -56,13 +118,24 @@ class ProgressManager {
   }
 
 
+  // مسح تقدم لعبة
   static Future<void> resetProgress(
     String gameName,
   ) async {
 
     final prefs = await SharedPreferences.getInstance();
 
-    await prefs.remove(gameName);
+    await prefs.remove(
+      "${gameName}_progress",
+    );
+
+    await prefs.remove(
+      "${gameName}_stars",
+    );
+
+    await prefs.remove(
+      "${gameName}_completed",
+    );
 
   }
 
