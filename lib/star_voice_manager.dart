854345dce66bc0d1778faad527318c9c13cc6db9
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-20 17:27:58 +0200

    Update star_voice_manager.dart

--- lib/star_voice_manager.dart
+++ lib/star_voice_manager.dart
@@ -1,23 +1,50 @@
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/foundation.dart';
 
 class StarVoiceManager {
   StarVoiceManager._();
 
   static final AudioPlayer _player = AudioPlayer();
 
+  // هل النجمة تتحدث؟
+  static bool _isTalking = false;
+
+  static bool get isTalking => _isTalking;
+
+  // إشعار للشاشات عند بدء وانتهاء الكلام
+  static final ValueNotifier<bool> talkingNotifier =
+      ValueNotifier(false);
+
   static Future<void> stop() async {
     await _player.stop();
+
+    _isTalking = false;
+    talkingNotifier.value = false;
   }
 
   static Future<void> _play(String fileName) async {
     try {
+      // إيقاف أي صوت سابق
       await _player.stop();
+
+      // بدء الكلام
+      _isTalking = true;
+      talkingNotifier.value = true;
 
       await _player.play(
         AssetSource("sounds/star/$fileName"),
       );
+
+      // عند انتهاء الصوت
+      _player.onPlayerComplete.first.then((_) {
+        _isTalking = false;
+        talkingNotifier.value = false;
+      });
     } catch (e) {
-      print("Star Voice Error: $e");
+      debugPrint("Star Voice Error: $e");
+
+      _isTalking = false;
+      talkingNotifier.value = false;
     }
   }
 
@@ -50,7 +77,4 @@ class StarVoiceManager {
   static Future<void> goodbye() async {
     await _play("goodbye.mp3");
   }
-
-  static Stream<void> get onComplete =>
-      _player.onPlayerComplete;
 }
