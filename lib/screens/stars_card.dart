0183db4981b8a20bfa61936986992c2d0380ea20
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-20 14:47:50 +0200

    Update stars_card.dart

--- lib/screens/stars_card.dart
+++ lib/screens/stars_card.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'happy_star.dart';
 
 class StarsCard extends StatefulWidget {
 
@@ -94,217 +95,38 @@ class _StarsCardState extends State<StarsCard>
 
 
           // ⭐ النجمة الشخصية
+// ⭐ النجمة الشخصية الجديدة
 
-          AnimatedBuilder(
+AnimatedBuilder(
+  animation: controller,
 
-            animation: controller,
+  builder: (context, child) {
 
-            builder:
-                (context, child) {
+    final double jump =
+        sin(controller.value * pi) * 8;
 
+    return Transform.translate(
 
-              final double jump =
+      offset: Offset(0, -jump),
 
-                  sin(controller.value * pi)
+      child: Transform.rotate(
 
-                      * 8;
+        angle:
+            sin(controller.value * pi) * 0.12,
 
+        child: child,
 
-              return Transform.translate(
+      ),
 
-                offset:
-                    Offset(0, -jump),
+    );
 
+  },
 
-                child:
-                    Transform.rotate(
+  child: HappyStar(
+    size: 90,
+  ),
 
-                  angle:
-
-                      sin(controller.value * pi)
-
-                          * 0.12,
-
-
-                  child: child,
-
-                ),
-
-              );
-
-            },
-
-
-            child: Stack(
-
-              alignment:
-                  Alignment.center,
-
-
-              children: [
-
-
-                // ✨ الوهج
-
-                Container(
-
-                  width: 95,
-
-                  height: 95,
-
-
-                  decoration:
-                      BoxDecoration(
-
-                    shape:
-                        BoxShape.circle,
-
-
-                    boxShadow: [
-
-                      BoxShadow(
-
-                        color:
-
-                            Colors.amber
-
-                                .withOpacity(0.6),
-
-
-                        blurRadius:
-                            25,
-
-
-                        spreadRadius:
-                            5,
-
-                      ),
-
-                    ],
-
-                  ),
-
-                ),
-
-
-
-                // ⭐ جسم النجمة
-
-                Container(
-
-                  width: 80,
-
-                  height: 80,
-
-
-                  decoration:
-                      BoxDecoration(
-
-
-                    shape:
-                        BoxShape.circle,
-
-
-                    gradient:
-                        const LinearGradient(
-
-                      colors: [
-
-                        Color(0xfffff176),
-
-                        Color(0xffff9800),
-
-                      ],
-
-                      begin:
-                          Alignment.topLeft,
-
-
-                      end:
-                          Alignment.bottomRight,
-
-                    ),
-
-
-
-                    border:
-                        Border.all(
-
-                      color:
-                          Colors.white,
-
-                      width:
-                          3,
-
-                    ),
-
-                  ),
-
-
-
-                  child:
-                      const Center(
-
-                    child:
-                        Text(
-
-                      "⭐",
-
-                      style:
-                          TextStyle(
-
-                        fontSize:
-                            55,
-
-                      ),
-
-                    ),
-
-                  ),
-
-                ),
-
-
-
-                // 😊 الوجه
-
-                const Positioned(
-
-                  bottom:
-                      18,
-
-
-                  child:
-                      Text(
-
-                    "😊",
-
-                    style:
-                        TextStyle(
-
-                      fontSize:
-                          22,
-
-                    ),
-
-                  ),
-
-                ),
-
-
-              ],
-
-            ),
-
-          ),
-
-
-
-          const SizedBox(
-
-            width: 18,
-
-          ),
+),
 
 
 
