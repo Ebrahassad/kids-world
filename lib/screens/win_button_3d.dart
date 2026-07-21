5f7ed6bb3e563a476e4af471c054140f5107945c
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-21 05:06:54 +0200

    Update win_button_3d.dart

--- lib/screens/win_button_3d.dart
+++ lib/screens/win_button_3d.dart
@@ -1,56 +1,236 @@
 import 'package:flutter/material.dart';
 
-class WinButton3D extends StatelessWidget {
+class WinButton3D extends StatefulWidget {
+
   final String text;
   final IconData icon;
   final Color color;
   final VoidCallback onPressed;
 
   const WinButton3D({
+
     super.key,
+
     required this.text,
+
     required this.icon,
+
     required this.color,
+
     required this.onPressed,
+
   });
+
+
+  @override
+  State<WinButton3D> createState() =>
+      _WinButton3DState();
+
+}
+
+
+
+class _WinButton3DState
+    extends State<WinButton3D> {
+
+
+  bool pressed = false;
+
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 280,
-      height: 68,
-      child: ElevatedButton.icon(
-        onPressed: onPressed,
-        icon: Icon(
-          icon,
-          size: 30,
-          color: Colors.white,
-        ),
-        label: Text(
-          text,
-          style: const TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.w900,
-            color: Colors.white,
-            letterSpacing: 0.5,
-            shadows: [
-              Shadow(
-                offset: Offset(0, 2),
-                blurRadius: 2,
-                color: Colors.black38,
+
+
+    return GestureDetector(
+
+      onTapDown: (_) {
+
+        setState(() {
+
+          pressed = true;
+
+        });
+
+      },
+
+
+      onTapUp: (_) {
+
+        setState(() {
+
+          pressed = false;
+
+        });
+
+        widget.onPressed();
+
+      },
+
+
+      onTapCancel: () {
+
+        setState(() {
+
+          pressed = false;
+
+        });
+
+      },
+
+
+      child: AnimatedContainer(
+
+        duration:
+            const Duration(milliseconds: 100),
+
+
+        transform:
+
+            Matrix4.translationValues(
+
+              0,
+
+              pressed ? 6 : 0,
+
+              0,
+
+            ),
+
+
+
+        width: 280,
+
+        height: 68,
+
+
+
+        decoration: BoxDecoration(
+
+          color:
+              widget.color.withOpacity(0.7),
+
+
+          borderRadius:
+              BorderRadius.circular(22),
+
+
+          boxShadow: [
+
+            BoxShadow(
+
+              color: Colors.black26,
+
+              offset: Offset(
+                0,
+                pressed ? 2 : 8,
               ),
+
+              blurRadius: 6,
+
+            ),
+
+          ],
+
+        ),
+
+
+
+        child: Container(
+
+          margin:
+              const EdgeInsets.only(
+                bottom: 6,
+              ),
+
+
+          decoration: BoxDecoration(
+
+            color: widget.color,
+
+
+            borderRadius:
+                BorderRadius.circular(22),
+
+          ),
+
+
+
+          child: Row(
+
+            mainAxisAlignment:
+                MainAxisAlignment.center,
+
+
+            children: [
+
+
+              Icon(
+
+                widget.icon,
+
+                size: 32,
+
+                color: Colors.white,
+
+              ),
+
+
+
+              const SizedBox(
+                width: 12,
+              ),
+
+
+
+              Text(
+
+                widget.text,
+
+
+                style: const TextStyle(
+
+                  fontSize: 22,
+
+                  fontWeight:
+                      FontWeight.w900,
+
+
+                  color:
+                      Colors.white,
+
+
+                  shadows: [
+
+                    Shadow(
+
+                      offset:
+                          Offset(0,2),
+
+                      blurRadius:
+                          2,
+
+                      color:
+                          Colors.black38,
+
+                    ),
+
+                  ],
+
+                ),
+
+              ),
+
+
             ],
+
           ),
+
         ),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: color,
-          elevation: 10,
-          shadowColor: Colors.black45,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(22),
-          ),
-        ),
+
       ),
+
     );
+
   }
+
 }
