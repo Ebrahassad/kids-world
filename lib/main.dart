2bda207c667337e279bb25de47b27de63bc4ac48
Ebrahassad <ebrahassadi1@gmail.com>
2026-07-21 05:43:38 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
 import 'app.dart';
 
-void main() {
-  runApp(const KidsWorldApp());
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+
+  // تشغيل AdMob
+  await MobileAds.instance.initialize();
+
+
+  runApp(
+    const KidsWorldApp(),
+  );
+
 }
